a105e6ddbd801784ab7c8e2e3ed6c25dfea581cf
Philip Beadle <philip.beadle@live.com.au>
2016-05-10 21:53:22 +1000

    Trying out icons

--- app/src/users/Users.js
+++ app/src/users/Users.js
@@ -6,7 +6,11 @@ const URL_Round2_SHARE   = 'assets/svg/Roundicons-41.svg';
 const URL_Round3_SHARE   = 'assets/svg/Roundicons-03.svg';
 const URL_Round4_SHARE   = 'assets/svg/Roundicons-04.svg';
 const URL_Round5_SHARE   = 'assets/svg/Roundicons-05.svg';
-
+const URL_sketch_SHARE   = 'assets/svg/Roundicons-39.svg';
+const URL_fair_SHARE   = 'assets/svg/Roundicons-17.svg';
+const URL_cake_SHARE   = 'assets/svg/Roundicons-08.svg';
+const URL_thongs_SHARE   = 'assets/svg/Roundicons-42.svg';
+const URL_skate_SHARE   = 'assets/svg/Roundicons-03.svg';
 // Load the custom app ES6 modules
 
 import UsersController from 'users/UsersController'
@@ -39,8 +43,12 @@ let moduleName = angular
           .icon('round4' ,URL_Round4_SHARE, 24)
           .icon('round5' ,URL_Round5_SHARE, 24)
           .icon('menu' ,URL_ICON_MENU, 24)
-          .icon('share',URL_ICON_SHARE, 24);
-
+          .icon('share',URL_ICON_SHARE, 24)
+          .icon('fair',URL_fair_SHARE, 24)
+          .icon('cake',URL_cake_SHARE, 24)
+          .icon('thongs',URL_thongs_SHARE, 24)
+          .icon('skate',URL_skate_SHARE, 24)
+          .icon('sketch',URL_sketch_SHARE, 24);
       })
       .name;
 
